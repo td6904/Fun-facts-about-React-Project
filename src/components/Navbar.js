5b48a9c1c4f06cd0bfdc29a726f0736d7a281193
Tyler Durnion <td665pro@pm.me>
2023-07-27 13:23:22 +0200

    Updated CSS test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,22 @@ export default function Navbar(props) {
   return (
     <div className={props.darkMode ? "dark" : ""}>
       <nav>
-        <img className="logo" src={logo} alt="logo" />
-        <h3 className="main-title">ReactFacts</h3>
+        <div className="responsive">
+          <div className="responsive-element-full-logo">
+            <img className="logo" src={logo} alt="logo" />
+            <h3 className="main-title">ReactFacts</h3>
+          </div>
 
-        <div className="toggler">
-          <p className="toggler-light">Light</p>
-          <label className="toggler-slider">
-            <input type="checkbox" onClick={props.toggleDarkMode}/>
-            <div className="toggler-slider-circle"></div>
-          </label>
-          <p className="toggler-dark">Dark</p>
+          <div className="responsive-element">
+            <div className="toggler">
+              <p className="toggler-light">Light</p>
+              <label className="toggler-slider">
+                <input type="checkbox" onClick={props.toggleDarkMode} />
+                <div className="toggler-slider-circle"></div>
+              </label>
+              <p className="toggler-dark">Dark</p>
+            </div>
+          </div>
         </div>
       </nav>
     </div>
